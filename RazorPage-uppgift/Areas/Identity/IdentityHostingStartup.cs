a94afe092d97aed2b606ae42be610a986301f6ce
olliver <olliver@OLLIVERS-PC>
2021-03-31 09:38:45 +0200

    Added Partial Identity Scaffolding

---
+++ RazorPage-uppgift/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPage_uppgift.Data;
+
+[assembly: HostingStartup(typeof(RazorPage_uppgift.Areas.Identity.IdentityHostingStartup))]
+namespace RazorPage_uppgift.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- RazorPage-uppgift/Data/RazorPage_uppgiftContext.cs
+++ RazorPage-uppgift/Data/RazorPage_uppgiftContext.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using RazorPage_uppgift.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace RazorPage_uppgift.Data
 {
-    public class RazorPage_uppgiftContext : DbContext
+    public class RazorPage_uppgiftContext : IdentityDbContext  //DbContext
     {
         public RazorPage_uppgiftContext (DbContextOptions<RazorPage_uppgiftContext> options)
             : base(options)
